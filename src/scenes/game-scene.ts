59ca230de7eebd463e27017703f887e589f54ffd
Joe Bustamante <joe.bustamante@atomicobject.com>
2019-04-26 23:45:59 -0400

    Add column to display currencies to buy

--- src/scenes/game-scene.ts
+++ src/scenes/game-scene.ts
@@ -1,4 +1,3 @@
-
 const sceneConfig: Phaser.Scenes.Settings.Config = {
   active: false,
   visible: false,
@@ -19,15 +18,41 @@ export class GameScene extends Phaser.Scene {
   }
 }
 
+interface Currency {
+  name: string;
+  currentValue: number;
+  amountOwned: number;
+}
+
+const currencies: Currency[] = [
+  { name: 'Dullers', currentValue: 43, amountOwned: 7 },
+  { name: 'When', currentValue: 11, amountOwned: 992 },
+  { name: 'Prawns', currentValue: 52, amountOwned: 0 },
+  { name: 'Pestos', currentValue: 24, amountOwned: 83 },
+];
+
 const createExchangeInterface = (scene: Phaser.Scene) => {
   createBuyInterface(scene);
   createSellInterface(scene);
 };
 
 const createBuyInterface = (scene: Phaser.Scene) => {
-  scene.add.text(100, 50, 'Buy');
+  scene.add.text(150, 100, 'BUY');
+
+  const nameColumnX = 100;
+  const currentValueColumnX = 300;
+  const headerColumnY = 200;
+
+  scene.add.text(nameColumnX, headerColumnY, 'Name');
+  const currentValueText = scene.add.text(currentValueColumnX, headerColumnY, 'Current Value');
+  const valueX = currentValueText.width / 2;
+
+  currencies.forEach((currency, index) => {
+    scene.add.text(nameColumnX, 300 + (50 * index), currency.name);
+    scene.add.text(currentValueColumnX + valueX, 300 + (50 * index), `${currency.currentValue}`);
+  });
 };
 
 const createSellInterface = (scene: Phaser.Scene) => {
-  scene.add.text(400, 50, 'Sell');
+  scene.add.text(750, 100, 'SELL');
 };
