94db96045376ab8f2f62a598eaab70015ffbdc46
Jason Porritt <jason.porritt@atomicobject.com>
2019-04-27 11:29:24 -0400

    Maybe functional fluctuating currency rates?

--- src/domain/trading.ts
+++ src/domain/trading.ts
@@ -1,9 +1,11 @@
 import * as Phaser from 'phaser';
 
+
 export enum DomainEvents {
   tradeCompleted = "domain.tradeCompleted",
   tradeFailed = "domain.tradeFailed",
   accountBalanceChanged = "domain.accountBalanceChanged",
+  exchangeRatesChanged = "domain.exchangeRatesChanged",
 }
 export enum DomainErrors {
   tradeFailed_InsufficientFunds = "Insufficient Funds"
@@ -31,14 +33,25 @@ export type Trade = {
   destinationCurrency: Currency, sourceAmount: number, destinationAmount: number,
   exchangeRate: number
 }
+export type DomainState = {
+  tradeLedger: TradeLedger,
+  tradeCurrencies: Currency[],
+  tradeAccounts: Account[],
+  rootCurrency: Currency,
+  rootAccount: Account
+}
 
 export function createAccount(name: string, startingBalance: number, currency: Currency): Account {
-  return {
+  let newAccount =  {
     name: name,
     currency: currency,
     balance: startingBalance,
-    ledger: [{amount: startingBalance, transactionType: "Credit"}]
+    ledger: []
   }
+  if (startingBalance > 0) {
+    newAccount.ledger.push({amount: startingBalance, transactionType: "Credit" });
+  }
+  return newAccount;
 }
 
 export function recordTrade(source: Account, destination: Account, sourceAmount: number, sourceToDestinationExchangeRate: number, tradeLedger: TradeLedger, eventEmitter: Phaser.Events.EventEmitter) {
@@ -63,4 +76,35 @@ export function recordTrade(source: Account, destination: Account, sourceAmount:
   } else {
     eventEmitter.emit(DomainEvents.tradeFailed, DomainErrors.tradeFailed_InsufficientFunds);
   }
+}
+
+const MIN_STARTING_EXCHANGE_RATE = 0.25;
+const MAX_STARTING_EXCHANGE_RATE = 4;
+
+function randomDecimalBetween(min: number, max: number): number {
+  return Math.random() * (max - min) + min;
+}
+export function initState(rootCurrencyName: string, rootCurrencyStartingAmount: number, currencyNames: string[]): DomainState {
+  let currencies: Currency[] = currencyNames.map(n => {
+    return {name: n, exchangeRate: randomDecimalBetween(MIN_STARTING_EXCHANGE_RATE, MAX_STARTING_EXCHANGE_RATE) }
+  });
+  let accounts: Account[] = currencies.map(c => {
+    return createAccount(c.name, 0, c);
+  });
+  let rootCurrency = { name: rootCurrencyName, exchangeRate: 1 };
+
+  return {
+    tradeCurrencies: currencies,
+    tradeAccounts: accounts,
+    tradeLedger: { trades: [] },
+    rootCurrency: rootCurrency,
+    rootAccount: createAccount(rootCurrencyName, rootCurrencyStartingAmount, rootCurrency)
+  }
+}
+
+export function runCurrencyFluctuations(state: DomainState, eventEmitter: Phaser.Events.EventEmitter) {
+  state.tradeCurrencies.forEach(currency => {
+    currency.exchangeRate += randomDecimalBetween(-0.5, 0.5);
+  });
+  eventEmitter.emit(DomainEvents.exchangeRatesChanged);
 }
