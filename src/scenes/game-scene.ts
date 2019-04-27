2c5d45e08d016a151ea6fb4d4f36c14d306a512b
Joe Bustamante <joe.bustamante@atomicobject.com>
2019-04-27 10:20:42 -0400

    Make currency displays update on change

--- src/scenes/game-scene.ts
+++ src/scenes/game-scene.ts
@@ -4,64 +4,91 @@ const sceneConfig: Phaser.Scenes.Settings.Config = {
   key: 'Game',
 };
 
-export class GameScene extends Phaser.Scene {
-  constructor() {
-    super(sceneConfig);
-  }
-
-  public create() {
-    createExchangeInterface(this);
-  }
-
-  public update() {
-    // TODO
-  }
-}
-
 interface Currency {
   name: string;
   currentValue: number;
   amountOwned: number;
 }
 
-const currencies: Currency[] = [
-  { name: 'Dullers', currentValue: 43, amountOwned: 7 },
-  { name: 'When', currentValue: 11, amountOwned: 992 },
-  { name: 'Prawns', currentValue: 52, amountOwned: 0 },
-  { name: 'Pestos', currentValue: 24, amountOwned: 83 },
-];
+export class GameScene extends Phaser.Scene {
+  currencies: Currency[];
+  currencyDisplay: Array<{ currencyName: string, valueText: Phaser.GameObjects.Text }>;
 
-const createExchangeInterface = (scene: Phaser.Scene) => {
-  createBuyInterface(scene);
-  createSellInterface(scene);
-};
+  buyHeaderStyle = { fontSize: '32px', color: '#44FF44' };
+  columnHeaderStyle = { fontSize: '24px', color: '#FFFFFF' };
+  currencyStyle = { fontSize: '20px', color: '#888888' };
+  nameColumnX = 100;
+  currentValueColumnX = 300;
+  buyColumnX = 450;
 
-const createBuyInterface = (scene: Phaser.Scene) => {
-  const buyHeaderStyle = { fontSize: '32px', color: '#44FF44' };
-  const columnHeaderStyle = { fontSize: '24px', color: '#FFFFFF' };
-  const currencyStyle = { fontSize: '20px', color: '#888888' };
+  headerColumnY = 200;
 
-  scene.add.text(150, 100, 'BUY', buyHeaderStyle);
+  constructor() {
+    super(sceneConfig);
+  }
 
-  const nameColumnX = 100;
-  const currentValueColumnX = 300;
-  const buyColumnX = 450;
+  public create() {
+    this.currencies = [
+      { name: 'Dullers', currentValue: 43, amountOwned: 7 },
+      { name: 'When', currentValue: 11, amountOwned: 992 },
+      { name: 'Prawns', currentValue: 52, amountOwned: 0 },
+      { name: 'Pestos', currentValue: 24, amountOwned: 83 },
+    ];
 
-  const headerColumnY = 200;
+    this.currencyDisplay = [];
 
-  scene.add.text(nameColumnX, headerColumnY, 'NAME', columnHeaderStyle);
-  const currentValueText = scene.add.text(currentValueColumnX, headerColumnY, 'VALUE', columnHeaderStyle);
-  const valueX = currentValueText.width / 2;
+    this.createExchangeInterface(this);
+  }
 
-  currencies.forEach((currency, index) => {
-    scene.add.text(nameColumnX, 250 + (50 * index), currency.name, currencyStyle);
-    scene.add.text(currentValueColumnX + valueX, 250 + (50 * index), `${currency.currentValue}`, currencyStyle);
-    scene.add.text(buyColumnX, 250 + (50 * index), '+', currencyStyle);
-  });
-};
+  public update() {
+    // randomly change currency values to test
+    this.currencies.forEach((currency) => {
+      const amount = Math.random() *  11;
+      const shouldBeNegative = Math.random() < 0.4;
 
-const createSellInterface = (scene: Phaser.Scene) => {
-  const sellHeaderStyle = { fontSize: '32px', color: '#FF4444' };
+      const amountToChange = Math.floor(shouldBeNegative ? -amount : amount);
 
-  scene.add.text(750, 100, 'SELL', sellHeaderStyle);
-};
+      currency.currentValue += amountToChange;
+    });
+
+    this.updateCurrencyDisplay();
+  }
+
+  private updateCurrencyDisplay() {
+    if (this.currencyDisplay.length > 0) {
+      this.currencyDisplay.forEach((display, index) => {
+        const correspondingCurrency = this.currencies[index]; // TODO - make this better
+        display.valueText.setText(`${correspondingCurrency.currentValue}`);
+      });
+    }
+  }
+
+  private createExchangeInterface = (scene: Phaser.Scene) => {
+    this.createBuyInterface();
+    this.createSellInterface();
+  }
+
+  private createBuyInterface = () => {
+
+    this.add.text(150, 100, 'BUY', this.buyHeaderStyle);
+
+
+    this.add.text(this.nameColumnX, this.headerColumnY, 'NAME', this.columnHeaderStyle);
+    this.add.text(this.currentValueColumnX, this.headerColumnY, 'VALUE', this.columnHeaderStyle);
+
+    this.currencies.forEach((currency, index) => {
+      this.add.text(this.nameColumnX, 250 + (50 * index), currency.name, this.currencyStyle);
+      const valueText = this.add.text(this.currentValueColumnX, 250 + (50 * index), `${currency.currentValue}`, this.currencyStyle);
+      this.add.text(this.buyColumnX, 250 + (50 * index), '+', this.currencyStyle);
+
+      this.currencyDisplay.push({ currencyName: currency.name, valueText });
+    });
+
+  }
+
+  private createSellInterface = () => {
+    const sellHeaderStyle = { fontSize: '32px', color: '#FF4444' };
+
+    this.add.text(750, 100, 'SELL', sellHeaderStyle);
+  }
+}
