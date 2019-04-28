a998af530542a873d02efb37948d24be3b0ae632
Dan Kelch <dan.kelch@atomicobject.com>
2019-04-28 09:57:41 -0400

    Add cursor to text box

--- src/components/input-box.ts
+++ src/components/input-box.ts
@@ -8,14 +8,21 @@ export const createInputBox = (scene: Phaser.Scene, x: number, y: number, callba
   };
 
   const rectangleElements = addRectangle(scene, x, y, 150, 30, Styles.foregroundColorHex);
+  const cursor = scene.add.rectangle(x + 8, y + 5, 10, 20, Styles.textColorHex).setOrigin(0,0);
+  cursor.setVisible(false);
   const inputBox = rectangleElements[0];
   inputBox.setInteractive();
 
-  const textField = scene.add.text(x + Styles.offset, y + Styles.offset / 2, '', { color: Styles.textColor });
+  const textField = scene.add.text(x + 5, y + Styles.offset / 2, '', { color: Styles.textColor });
 
   scene.input.on('pointerup', (pointer, currentlyOver: any[]) => {
     console.log('pointer is up!', pointer, currentlyOver);
     textFieldState.isEditable = _.some(currentlyOver, (gameObject) => gameObject === inputBox || gameObject === textField);
+    if (textFieldState.isEditable) {
+      cursor.setVisible(true);
+    } else {
+      cursor.setVisible(false);
+    }
     console.log(textFieldState.isEditable)
   });
 
@@ -31,11 +38,13 @@ export const createInputBox = (scene: Phaser.Scene, x: number, y: number, callba
     if (event.keyCode === 8 && textField.text.length > 0)
     {
       textField.text = textField.text.substr(0, textField.text.length - 1);
+      cursor.setX(textField.x + textField.width + 3);
       callback(textField.text);
     }
     else if (textField.text.length < maxCharacterLength && (event.keyCode === 32 || (event.keyCode >= 48 && event.keyCode < 90)))
     {
       textField.text += event.key;
+      cursor.setX(textField.x + textField.width + 3);
       callback(textField.text);
     }
 
