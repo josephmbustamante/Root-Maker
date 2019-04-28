95d741526c354758057e9167ff093a5a72689cb1
Joe Bustamante <joe.bustamante@atomicobject.com>
2019-04-27 23:00:29 -0400

    Create input box component

--- src/components/rectangle.ts
+++ src/components/rectangle.ts
@@ -6,12 +6,18 @@ export const addRectangle = (scene: Phaser.Scene, x: number, y: number, width: n
 
   const box = scene.add.rectangle(x, y, width, height, fillColor, fillAlpha).setOrigin(0,0);
   // top line
-  scene.add.line(0, 0, x - 1, y, x + width, y, Styles.detailDarkColorHex).setOrigin(0, 0);
+  const topLine = scene.add.line(0, 0, x - 1, y, x + width, y, Styles.detailDarkColorHex).setOrigin(0, 0);
   // left line
-  scene.add.line(0, 0, x, y - 1, x, y + height + 1, Styles.detailDarkColorHex).setOrigin(0, 0);
+  const leftLine = scene.add.line(0, 0, x, y - 1, x, y + height + 1, Styles.detailDarkColorHex).setOrigin(0, 0);
   // right line
-  scene.add.line(0, 0, x + width, y - 1, x + width, y + height + 1, Styles.detailLightColorHex).setOrigin(0, 0);
+  const rightLine = scene.add.line(0, 0, x + width, y - 1, x + width, y + height + 1, Styles.detailLightColorHex).setOrigin(0, 0);
   // bottom line
-  scene.add.line(0, 0, x + 1, y + height, x + width, y + height, Styles.detailLightColorHex).setOrigin(0, 0);
-  return box;
+  const bottomLine = scene.add.line(0, 0, x + 1, y + height, x + width, y + height, Styles.detailLightColorHex).setOrigin(0, 0);
+  return [
+    box,
+    topLine,
+    leftLine,
+    rightLine,
+    bottomLine,
+  ];
 }
